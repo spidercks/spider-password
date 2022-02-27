91dfed51b0352eceb8b41074db2eb1f91abdc692
spidercks <98835437+spidercks@users.noreply.github.com>
2022-02-27 07:32:47 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -28,7 +28,7 @@ def tam_senhas():
 
 
 def gerador_senhas(quant, tam):
-    cabecalho("GERANDO SENHAS...")
+    cabecalho("GERANDO SENHAS/WORDLIST...")
     sleep(1)
 
     strings = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","&","*")
